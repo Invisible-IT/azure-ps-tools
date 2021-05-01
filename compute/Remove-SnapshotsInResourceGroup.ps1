d541c43f9f3a8eb8433b564da05aa11c08035fc1
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2021-05-01 13:48:11 +0200

    Add removal script for snapshots

--- compute/Remove-SnapshotsInResourceGroup.ps1
+++ compute/Remove-SnapshotsInResourceGroup.ps1
@@ -1,16 +1,20 @@
 [CmdletBinding(DefaultParameterSetName)]
 param (
-        
+
 )
 
 $VmResourceGroupName = "INVISIBLEIT-TEST-LINUX-VM-RG"
 
+Write-Verbose "Retreiving resource list from $VmResourceGroupName"
 $resources = Get-AzResource -ResourceGroupName $VmResourceGroupName
 
 foreach ($resource in $resources) {
     if ($resource.ResourceType -eq "Microsoft.Compute/snapshots") {
+        Write-Verbose "Removing snapshot $($resource.ResourceGroupName) / $($resource.ResourceName)"
+
         Remove-AzResource -ResourceGroupName $resource.ResourceGroupName `
                             -ResourceName $resource.ResourceName `
+                            -ResourceType $resource.ResourceType `
                             -Force
     }
 }
